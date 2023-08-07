4380e8cabddf9cd4ea61e5f45745525b8e08bee8
shunmugam-wise <shunmugam@solidfi.com>
2023-08-07 20:16:26 +0530

    fix

---
+++ static/js/6938.2614e962.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdrop_in_ui=self.webpackChunkdrop_in_ui||[]).push([[6938],{6938:function(e,n,i){i.r(n);var r=i(2791),u=i(7689),t=i(9584),s=i(184),l=r.lazy((function(){return Promise.all([i.e(527),i.e(1951),i.e(1174),i.e(8833)]).then(i.bind(i,8833))}));n.default=function(){return(0,s.jsx)(u.Z5,{children:(0,s.jsx)(u.AW,{path:"",element:(0,s.jsx)(t.Z,{children:(0,s.jsx)(l,{})})})})}}}]);
+//# sourceMappingURL=6938.2614e962.chunk.js.map
